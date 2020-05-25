86b1205d939ea611be6f567e51a2c32f1f769089
Antony Sanders <antony.devon92@gmail.com>
2020-05-25 05:52:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,11 @@ function unleashDog(dogName, dogBreed) {
 const routine = [wakeDog, leashDog, walkDog, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed){
-   return routine.map(f => f(dogName, dogBreed))
+   let i = 0; // the initialization moved OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+ 
+  return gifts;
 } 
