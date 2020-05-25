1ea226f021b4668a2971d85f4b994a1b4c959449
Antony Sanders <antony.devon92@gmail.com>
2020-05-25 05:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function wakeDog() {
   console.log("Wake Byron the poodle");
 }
+
+function leasheDog() {
+  console.log("Wake Byron the poodle");
+}
+
+function walkDog() {
+  console.log("Wake Byron the poodle");
+}
+
+function throwFrisbee() {
+  console.log("Wake Byron the poodle");
+}
+
+function walkHome() {
+  console.log("Wake Byron the poodle");
+}
+
+function unleashDog() {
+  console.log("Wake Byron the poodle");
+}
