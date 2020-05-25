73d39ba52eb953bb7d4b9d85235d1840ac23a4a5
Antony Sanders <antony.devon92@gmail.com>
2020-05-25 05:32:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
 }
 
 function leashDog(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`
 }
 
 function walkDog(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return
 }
 
 function throwFrisbee(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return
 }
 
 function walkHome(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return
 }
 
 function unleashDog(dogName, dogBreed) {
   console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return
 }
 
 var routine = [wakeDog, leashDog, walkDog, throwFrisbee, walkHome, unleashDog]
