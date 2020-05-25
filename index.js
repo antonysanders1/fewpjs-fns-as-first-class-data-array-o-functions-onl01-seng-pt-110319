2e9bea38cd370c0a23a532e46111368b107b6106
Antony Sanders <antony.devon92@gmail.com>
2020-05-25 06:01:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,11 +33,10 @@ const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashD
 function exerciseDog(dogName, dogBreed){
    let i = 0; // the initialization moved OUTSIDE the body of the loop!
   while (i < routine.length) {
-    //console.log(routine[i](dogName, dogBreed));
-    i++; // the iteration moves INSIDE the body of the loop!
-    return routine[i](dogName, dogBreed);
+    console.log(routine[i](dogName, dogBreed));
     
+    i++; // the iteration moves INSIDE the body of the loop!
   }
  
-  return routine;
+  return routine.all;
 } 
